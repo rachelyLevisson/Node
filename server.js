59d47691de713be91f73f1aca8a832e7537f1aa0
לויסון <@DESKTOP-9G3ER3M>
2025-02-07 00:18:03 +0200

    app -> service

--- server.js
+++
@@ -1,8 +0,0 @@
-import renderApi from '@api/render-api';
-
-renderApi.auth('rnd_xmrXh9GTY3ECKGeztnqj2WLwOVy9');
-renderApi.listServices({includePreviews: 'true', limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
-
-  
